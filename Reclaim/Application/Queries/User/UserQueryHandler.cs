efddd7ea2365cb3da0c91bf14bb4d9caabd2a06e
Julia Ilasova <1julka1il@gmail.com>
2025-05-04 11:08:43 +0200

    finish query for user

--- Reclaim/Application/Queries/User/UserQueryHandler.cs
+++ Reclaim/Application/Queries/User/UserQueryHandler.cs
@@ -1,11 +1,19 @@
 ﻿using Reclaim.Domain.Entities.Read;
+using Reclaim.Domain.Exceptions;
+using Reclaim.Infrastructure.Repositories.Read.Interfaces;
 
 namespace Reclaim.Application.Queries.User;
 
-public class UserQueryHandler : IQueryHandler<GetUserByIdQuery, UserReadEntity>
+public class UserQueryHandler(IUserReadRepository userReadRepository) : IQueryHandler<GetUserByIdQuery, UserReadEntity>
 {
     public async Task<UserReadEntity> HandleAsync(GetUserByIdQuery query)
     {
-        throw new NotImplementedException();
+        var user = await userReadRepository.GetByIdAsync(query.UserId);
+        if (user is null)
+        {
+            throw new NotFoundException($"User with ID {query.UserId} not found.");
+        }
+
+        return user;
     }
 }
