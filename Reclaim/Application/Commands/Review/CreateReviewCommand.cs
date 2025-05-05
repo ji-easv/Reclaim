12e9efec85cbcb3395ec3318701b594806a08846
MatejMa2ur <matejmazur@outlook.com>
2025-05-05 23:13:52 +0200

    Add IsDeleted property and ReviewMapper for review handling
    Introduced the `IsDeleted` property in Review entities to support soft deletion. Added `ReviewMapper` to handle conversions between entities, DTOs, and commands. Enhanced `CreateReviewCommand` and `ReviewGetDto` to include new fields for comprehensive review management.

--- Reclaim/Application/Commands/Review/CreateReviewCommand.cs
+++ Reclaim/Application/Commands/Review/CreateReviewCommand.cs
@@ -4,4 +4,8 @@ namespace Reclaim.Application.Commands.Review;
 
 public class CreateReviewCommand : ICommand<ReviewWriteEntity>
 {
+    public required string Content { get; set; }
+    public required int Rating { get; set; }
+    public required string AuthorId { get; set; }
+    public required string SellerId { get; set; }
 }
